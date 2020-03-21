3bf231a3b0787b8c1122a264e955f82e76777ee7
Eric Anastas <aireq303@gmail.com>
2020-03-21 12:43:03 -0700

    Moved browser.close() into finally block

--- deploy.js
+++ deploy.js
@@ -15,62 +15,70 @@ const enableLogging = false;
     });
 
 
-    //To set window enviorment variables in powershell
-    //PowerSheell
-    //$env: GOOGLE_USER = 'alic@acme.com'
-    //$env: GOOGLE_PASS = '123456'
+    try {
+        //To set window enviorment variables in powershell
+        //PowerSheell
+        //$env: GOOGLE_USER = 'alic@acme.com'
+        //$env: GOOGLE_PASS = '123456'
 
-    const userName = process.env.GOOGLE_USER;
-    const password = process.env.GOOGLE_PASS;
+        
 
-    const page = await browser.newPage();
+        const userName = process.env.GOOGLE_USER;
+        const password = process.env.GOOGLE_PASS;
 
-    const authurl = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fscript.deployments%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fscript.projects%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fscript.webapp.deploy%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.file%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fservice.management%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Flogging.read%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform&response_type=code&client_id=1072944905499-vm2v2i5dvn0a0d2o4ca36i1vge8cvbn0.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob";
+        const page = await browser.newPage();
 
-    log("opening url, and waiting until networkidle0");
-    await page.goto(authurl, { waitUntil: "networkidle0" });
+        const authurl = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fscript.deployments%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fscript.projects%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fscript.webapp.deploy%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.file%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fservice.management%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Flogging.read%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform&response_type=code&client_id=1072944905499-vm2v2i5dvn0a0d2o4ca36i1vge8cvbn0.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob";
 
-    var emailSelector = "#identifierId"
-    if (headless) var emailSelector = "#Email"
+        log("opening url, and waiting until networkidle0");
+        await page.goto(authurl, { waitUntil: "networkidle0" });
 
-    log("waiting for login screen (#identifierId)");
-    await page.waitFor(emailSelector, { visible: true });
+        var emailSelector = "#identifierId"
+        if (headless) var emailSelector = "#Email"
 
-    log("entering username");
-    await page.type(emailSelector, userName);
+        log("waiting for login screen (#identifierId)");
+        await page.waitFor(emailSelector, { visible: true });
 
-    log("pressing enter");
-    await page.keyboard.press('Enter');
+        log("entering username");
+        await page.type(emailSelector, userName);
 
-    var pwSelector = "#password input"
-    if (headless) var pwSelector = "#Passwd"
+        log("pressing enter");
+        await page.keyboard.press('Enter');
 
-    log("waiting for password input");
-    await page.waitFor(pwSelector, { visible: true });
+        var pwSelector = "#password input"
+        if (headless) var pwSelector = "#Passwd"
 
-    log("entering password");
-    await page.type(pwSelector, password);
+        log("waiting for password input");
+        await page.waitFor(pwSelector, { visible: true });
 
-    log("pressing enter");
-    await page.keyboard.press('Enter');
+        log("entering password");
+        await page.type(pwSelector, password);
 
-    var approveButtonSelector = "#submit_approve_access:not([disabled])";
+        log("pressing enter");
+        await page.keyboard.press('Enter');
 
-    log("waiting for appove button");
-    await page.waitFor(approveButtonSelector, { visible: true });
+        var approveButtonSelector = "#submit_approve_access:not([disabled])";
 
-    log("clicking approve button");
-    await page.click(approveButtonSelector, { waitUntil:"networkidle0 "});
+        log("waiting for appove button");
+        await page.waitFor(approveButtonSelector, { visible: true });
 
-    log("waiting for grant code");
-    await page.waitFor("#view_container textarea", { visible: true });
-    const element = await page.$("#view_container textarea");
-    const code = await page.evaluate(element => element.textContent, element);
+        log("clicking approve button");
+        await page.click(approveButtonSelector, { waitUntil: "networkidle0 " });
 
-    log("code:" + code);
-    process.stdout.write(code);
+        log("waiting for grant code");
+        await page.waitFor("#view_container textarea", { visible: true });
+        const element = await page.$("#view_container textarea");
+        const code = await page.evaluate(element => element.textContent, element);
 
-    await browser.close();
+        log("code:" + code);
+        process.stdout.write(code);
+    }
+    finally {
+
+        await browser.close();
+
+    }
+
 })();
 
 function log(message) {
